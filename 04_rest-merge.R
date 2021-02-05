508baeef3ebc0ad2c58518fa7bf3e6e84053956c
Will Foran <willforan@gmail.com>
2021-02-04 22:23:47 -0500

    🍱 txt/AAL-subset7_roiroi-zscore.csv: roiroi corr values

--- 04_rest-merge.R
+++ 04_rest-merge.R
@@ -5,7 +5,7 @@
 #  make very (all roi-roi pairs as columns) dataframe
 #  and merge with demographics
 
-library(dplyr)
+suppressPackageStartupMessages({library(dplyr)})
 
 # N.B. should be sorted. so names match rois (even after unique)
 roi_names <- unique(read.table('masks/AAL3/AAL_subset7.nii.txt')$V2)
@@ -24,3 +24,4 @@ wide <-
 # add demographic info
 dmg <- read.csv('txt/id_dmg.csv') %>% rename(ses=timepoint)
 roiroi_dmg <- merge(wide, dmg, by=c("id","ses"))
+write.csv(roiroi_dmg, 'txt/AAL-subset7_roiroi-zscore.csv', row.names=F, quote=F)
